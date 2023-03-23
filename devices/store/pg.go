4e0963b8aef488c6c00a094dfd7a9c9eb0e7a72b
Chris Hern <christopher.t.hern@gmail.com>
2023-03-23 05:30:39 -0500

    refactor: made SQL queries easier to read

--- devices/store/pg.go
+++ devices/store/pg.go
@@ -33,8 +33,9 @@ func NewPGRepo(dsn string) (*PGRepo, error) {
 
 // NewDevice adds a device to DB.
 func (pr *PGRepo) NewDevice(newDevice models.NewDeviceRequest) (models.Device, error) {
-	query := `INSERT INTO devices (hostname, ip, clli) VALUES ($1, $2, $3)
-	RETURNING id;`
+	query := `-- insert one
+	INSERT INTO devices (hostname, ip, clli) 
+	VALUES ($1, $2, $3) RETURNING id;`
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	device := mapDeviceAttrs(newDevice)
@@ -46,7 +47,8 @@ func (pr *PGRepo) NewDevice(newDevice models.NewDeviceRequest) (models.Device, e
 
 // GetDeviceByIP queries the DB for the given IP.
 func (pr *PGRepo) GetDeviceByIP(ip string) (models.Device, error) {
-	query := `SELECT id, hostname, ip, clli FROM devices WHERE ip = $1`
+	query := `-- get one
+	SELECT id, hostname, ip, clli FROM devices WHERE ip = $1`
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	var dev models.Device
